d62b5a332d9310e1bd60d8f8466bfa293d41bc93
Kevin Angelo Apostol Espaldon <K.Espaldon@dcd.gov.ae>
2018-12-19 10:05:05 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,24 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = { currentMonth: {} };
+  }
   render() {
     return (
-      <div className="App">
+      <main className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
-      </div>
+        <Calendar />
+        <section className="App-appointments"></section>
+      </main>
+    );
+  }
+}
+
+class Calendar extends Component {
+  render() {
+    return (
+      <section className="App-calendar">
+
+      </section>
     );
   }
 }
